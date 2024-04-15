c8661177da66e9663a3ee1d17cf0693d1b98e9e6
Jin Ma <jinma@linux.alibaba.com>
2024-04-15 14:46:34 +0800

    update matrix intrinsic for new xuantie-gnu-toolchain.

--- demos/intrinsic_matmul/matmul.c
+++ demos/intrinsic_matmul/matmul.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-#include <riscv_matrix.h>
+#include <thead_matrix.h>
 #define N 16
 
-void __attribute__((inline))
-print_data(const char *fmt, mint32_t ma, mint32_t mb, mint32_t ans)
+void __attribute__((noinline))
+print_data(const char *fmt, mint32_t ma, mint32_t mb, mint32_t ans, mrow_t mrow, mcol_t mcol)
 {
   unsigned int row, col;
   int32_t tmp_ma[N];
@@ -12,29 +12,29 @@ print_data(const char *fmt, mint32_t ma, mint32_t mb, mint32_t ans)
 
   printf("%s:\n", fmt);
 
-  mst_i32_mi32(tmp_ma, 8, ma);
-  mst_i32_mi32(tmp_mb, 8, mb);
-  mst_i32_mi32(tmp_ans, 8, ans);
+  __riscv_th_mst(tmp_ma, 8, ma, mrow, mcol);
+  __riscv_th_mst(tmp_mb, 8, mb, mrow, mcol);
+  __riscv_th_mst(tmp_ans, 8, ans, mrow, mcol);
 
   printf("ma:\t\tmb:\t\tans:\n");
-  for (row = 0; row < 2; row++)
+  for (row = 0; row < mrow; row++)
   {
-    for (col = 0; col < 2; col++)
+    for (col = 0; col < mcol; col++)
     {
-      printf("%-3d ", tmp_ma[row + col]);
+      printf("%-3d ", tmp_ma[row * mrow + col]);
     }
     printf("\t");
-    for (col = 0; col < 2; col++)
+    for (col = 0; col < mcol; col++)
     {
-      printf("%-3d ", tmp_mb[row + col]);
+      printf("%-3d ", tmp_mb[row * mrow + col]);
     }
     printf("\t");
-    for (col = 0; col < 2; col++)
+    for (col = 0; col < mcol; col++)
     {
       if (tmp_ans[0] == 0)
-        printf("%-2d ", tmp_ans[row + col]);
+        printf("%-2d ", tmp_ans[row * mrow + col]);
       else
-        printf("%-2d = %-2d * %-2d  ", tmp_ans[row + col], tmp_ma[row + col], tmp_mb[row + col]);
+        printf("%-2d = %-2d * %-2d  ", tmp_ans[row * mrow + col], tmp_ma[row * mrow + col], tmp_mb[row * mrow + col]);
     }
     printf("\n");
   }
@@ -49,24 +49,18 @@ int main()
   int32_t z[N] = {0};
 
   uint8_t msize_m = 2;
-  uint8_t msize_n = 2;
-  uint16_t msize_k = 8; // sizeof(int32_t) * 2;
-  long stride = 8;      // sizeof(int32_t) * 2;
-
-  /* Configuration matrix size */
-  mcfgm(msize_m);
-  mcfgn(msize_n);
-  mcfgk(msize_k);
+  uint8_t msize_k = 2;
+  long stride = 2 * sizeof(int32_t); // sizeof(int32_t) * 2;
 
   /* init matrix value*/
-  mint32_t ma = mld_i32(x, stride);
-  mint32_t mb = mld_i32(y, stride);
-  mint32_t ans = mld_i32(z, stride);
+  mint32_t ma = __riscv_th_mld(x, stride, msize_m, msize_k);
+  mint32_t mb = __riscv_th_mld(y, stride, msize_m, msize_k);
+  mint32_t ans = __riscv_th_mld(z, stride, msize_m, msize_k);
 
-  print_data("Initial value of matrix", ma, mb, ans);
+  print_data("Initial value of matrix", ma, mb, ans, msize_m, msize_k);
 
-  ans = mmul_mi32(ma, mb);
-  print_data("Results of multiplication", ma, mb, ans);
+  ans = __riscv_th_mmul_mm(ma, mb, msize_m, msize_k);
 
+  print_data("Results of multiplication", ma, mb, ans, msize_m, msize_k);
   return 0;
 }
